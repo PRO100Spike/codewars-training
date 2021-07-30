bf1b61f7179d6ecff1deddeaa099dd4d29918ab5
Yakushew Anton <spike2work@gmail.com>
2021-07-30 16:27:55 +0300

    Update index.js

--- write-number-expanded-form/index.js
+++ write-number-expanded-form/index.js
@@ -1,10 +1,14 @@
 function expandedForm(num) {
   // Your code here
-  let str = num % 10 > 0 ? num % 10 : '';
+  let str = num % 10 > 0 ? num % 10  + '' : '';
   let level = 0;
 
   const rec = (p) => {
-    str = p % 10 > 0 ? `${(p % 10) * Math.pow(10, level + 1)} + ${str}` : str;
+    if (str !== '') {
+      str = p % 10 > 0 ? `${(p % 10) * Math.pow(10, level + 1)} + ${str}` : str;
+    } else {
+      str = p % 10 > 0 ? `${(p % 10) * Math.pow(10, level + 1)}` : str;
+    }
     level++;
     return p / 10 < 1 ? str : rec(Math.floor(p / 10));
   };
