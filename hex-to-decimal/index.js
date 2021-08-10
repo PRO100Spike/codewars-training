b405b8fdd87ed5d318e8c2f1b02c12e40b54c7f8
Yakushew Anton <spike2work@gmail.com>
2021-08-11 00:28:30 +0300

    Update index.js

--- hex-to-decimal/index.js
+++ hex-to-decimal/index.js
@@ -19,6 +19,24 @@ function hexToDec(hexString) {
     }, 0);
 }
 
+// or opt solution
+function hexToDec(hexString) {
+  //your code here
+  return hexString
+    .toUpperCase()
+    .split("")
+    .reduce((a, v, i) => {
+      return (
+        a +
+        (hexString[0] === "-" ? -1 : 1) *
+          (+v >= 0
+            ? +v * Math.pow(16, hexString.length - i - 1)
+            : (v.charCodeAt() - (v.charCodeAt() === 45 ? 45 : 55)) *
+              Math.pow(16, hexString.length - i - 1))
+      );
+    }, 0);
+}
+
 // or Best Practices
 function hexToDec(hexString) {
   return  parseInt(hexString, 16);
