08c32de83b0f66c51987555f914b1eeb65411f8c
Yakushew Anton <spike2work@gmail.com>
2021-08-30 10:22:56 +0300

    Create index.js

---
+++ find-multiples-number/index.js
@@ -0,0 +1,12 @@
+https://www.codewars.com/kata/58ca658cc0d6401f2700045f/train/javascript
+
+function findMultiples(integer, limit) {
+  //your code here
+  let multiples = Math.floor(limit / integer);
+  let res = [];
+  if (multiples <= 0) return 0
+  for (let i = 1; i <= multiples; i++) {
+    res.push(integer * i)
+  }
+  return res
+}
