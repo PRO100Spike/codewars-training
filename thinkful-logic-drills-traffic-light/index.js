d73a655ea14a779f637eb1e0184bdce605f9416e
Yakushew Anton <spike2work@gmail.com>
2021-10-26 12:22:49 +0300

    Create index.js

---
+++ thinkful-logic-drills-traffic-light/index.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/58649884a1659ed6cb000072/train/javascript
+
+function updateLight(current) {
+  
+  //your code here!
+  switch (current) {
+      case 'green':
+      return 'yellow'
+      case 'yellow':
+      return 'red'
+      default:
+      return 'green'
+  }
+
+}
