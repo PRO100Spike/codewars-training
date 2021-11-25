5ce2f331fec106f30a0ea75a42981e2d8de68c97
Yakushew Anton <spike2work@gmail.com>
2021-11-25 11:04:19 +0300

    Create index.js

---
+++ Leap Years/index.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/526c7363236867513f0005ca/train/javascript
+
+function isLeapYear(year) {
+  // TODO
+  return year % 4 === 0 || (year % 400 === 0 && year % 100 !== 0)
+}
