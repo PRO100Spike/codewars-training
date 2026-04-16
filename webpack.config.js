fad78a00335aeac6b9f8af1b6ea116a5c8ba90a6
procyon Spike <13spike@.mail.ru>
2026-04-16 16:29:11 +0300

    fix:rename dirs

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,56 @@
-import path from 'path';
-import webpack from 'webpack';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const __dirname = path.resolve();
-
-export default {
-  mode: 'development',
-  output: {
-    publicPath: '/',
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  entry: {
-    index: './index.js'
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    static: path.resolve(__dirname, './dist'),
-    open: true,
-    compress: true,
-    hot: false,
-    port: 8080,
-    historyApiFallback: true,
-    /*historyApiFallback: {
-      index: 'index.html'
-    }*/
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'Development',
-    }),
-    new webpack.ProvidePlugin({
-      process: 'process/browser',
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.md$/,
-        use: 'raw-loader',
-      },
+module.exports = {
+    mode: 'development',
+    output: {
+        publicPath: '/',
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    entry: {
+        index: './index.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: path.resolve(__dirname, './dist'),
+        open: true,
+        compress: true,
+        hot: false,
+        port: 8080,
+        historyApiFallback: true,
+        /*historyApiFallback: {
+          index: 'index.html'
+        }*/
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Development',
+        }),
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+        }),
     ],
-  },
+    module: {
+        rules: [
+            {
+                test: /\.m?js/,
+                resolve: {
+                    fullySpecified: false
+                },
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.md$/,
+                use: 'raw-loader',
+            },
+        ],
+    },
 };
